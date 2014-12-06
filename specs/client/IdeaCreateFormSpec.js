861134fdad8ae7eee820e9842ae2f271bdc0895d
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-12-06 18:47:23 -0500

    Create IdeaCreateForm react component and tests.

---
+++ specs/client/IdeaCreateFormSpec.js
@@ -0,0 +1,26 @@
+describe('Idea Form',function(){
+
+  var TestUtils,ideaCreateForm,ideaMockAction;
+
+  beforeEach(function(){
+    TestUtils = React.addons.TestUtils;
+    ideaCreateForm = React.createElement(app.IdeaCreateForm,null);
+    app.IdeaActions = {
+      create:function(text){
+        ideaMockAction = text;
+      }
+    };
+    ideaCreateForm = TestUtils.renderIntoDocument(ideaCreateForm);
+  });
+
+  it('should render the body',function(){
+    expect(ideaCreateForm.refs.body).to.be.ok();
+  });
+
+  it('should handle a click',function(){
+    ideaCreateForm.refs.body.getDOMNode().value = 'test';
+    TestUtils.Simulate.submit(ideaCreateForm.refs.form.getDOMNode());
+    expect(ideaMockAction).to.equal('test');
+  });
+
+});
