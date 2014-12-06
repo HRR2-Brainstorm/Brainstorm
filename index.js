86ff79c4ef9eb505e9082e97f1dbf77ef2958ec1
brettdewoody <brett@digitalwaxworks.com>
2014-12-06 11:29:13 -0700

    Add mongoose

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var app = require('./server/server.js');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/brainstormer');
 
-var port = 8000;
-app.listen(port, function() {
-  console.log('Server is listening on ' + port);
-});
+app.listen(8000);
