2ba197a8466ea64c0083c21d1c5d99372fbdda54
brettdewoody <brett@digitalwaxworks.com>
2014-12-06 12:18:08 -0700

    Clean up mongooose

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ var User = mongoose.model('User');
 var Idea = mongoose.model('Idea');
 var Room = mongoose.model('Room');
 
+<<<<<<< HEAD
+=======
+
+var port = 8000;
+app.listen(port, function() {
+  console.log('Server is listening on ' + port);
+});
+
+app.listen(8000);
+>>>>>>> Clean up mongooose
