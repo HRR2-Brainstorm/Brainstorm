53225129d44fba2b94a53e08ad6fbe4bdc5befa4
Jason Deppen <jDeppen@gmail.com>
2014-12-06 18:58:41 -0500

    Finish socket.io prep.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
+'use strict';
+
 var express = require('express');
 var mongoose = require('./db.js');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(client) {
+  console.log('io says "Client connected..."');
+
+  client.on('messages', function(data) {
+    console.log(data);
+    client.broadcast.emit('messages', data);
+  });
+
+  client.emit('messages', 'hello world');
+});
 
 require('./config/middleware')(app, express);
 
-module.exports = app;
+module.exports = server;
