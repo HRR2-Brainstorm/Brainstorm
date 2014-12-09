88fc50d26494960fef54807ddbc63e32360cd8dd
Gunnari Auvinen <gauvinen@gmail.com>
2014-12-09 02:06:46 -0500

    Add editing state and edit method for Idea component. Add form display if in editing state.

--- client/react/Idea.js
+++ client/react/Idea.js
@@ -1,7 +1,37 @@
 app.Idea = React.createClass({
+  getInitialState: function() {
+    return {
+      editing: false
+    };
+  },
+
+  componentDidMount: function() {
+    app.IdeaStore.addChangeListener(function() {
+      if(this.isMounted()) {
+        this.setState({editing: false});
+      }
+    }.bind(this))
+  },
+
   render: function() {
+    if (this.state.editing) {
+      var editForm = <app.IdeaForm editing="true" name={this.props.name} />
+    } else {
+      var editForm = undefined;
+    }
     return (
-      <h3 ref="body">{this.props.name}</h3>
+      <div>
+        <h3 ref="body">{this.props.name}</h3>
+        <button onClick={this.edit}>Edit Idea</button>
+        {editForm}
+      </div>
     );
+  },
+
+  edit: function(e) {
+    e.preventDefault();
+    if (this.isMounted()) {
+      this.setState({editing: !this.state.editing});
+    }
   }
 });
