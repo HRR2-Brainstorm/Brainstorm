32fb476f99a396ee5cdcd17d1d2ccaf7714785cb
Gunnari Auvinen <gauvinen@gmail.com>
2014-12-08 19:18:23 -0500

    Create ideaRoutes and add handling of 'POST' action.

---
+++ server/ideas/ideaRoutes.js
@@ -0,0 +1,7 @@
+var ideaController = require('./ideaController.js');
+
+module.exports = function (app) {
+  // app === userRouter injected from middlware.js
+  app.route('/')
+  .post(ideaController.newIdea)
+};
