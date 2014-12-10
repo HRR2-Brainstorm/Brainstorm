f015508ce411eb2ac44f89bbaf47dd46246e96c2
brettdewoody <brett@digitalwaxworks.com>
2014-12-10 00:13:06 -0700

    Remove className from button.

--- client/react/RoomCreateForm.js
+++ client/react/RoomCreateForm.js
@@ -12,7 +12,7 @@ app.RoomCreateForm = React.createClass({
   render: function(){
     return (
       <form className="pure-form pure-g" ref="form" onSubmit={this.handleSubmit}>
-        <input className="pure-input-1" type="text" ref="name" placeholder="Room" />
+        <input type="text" ref="name" placeholder="Room" />
         <button className="button-small pure-button pure-button-primary" type="submit" ref="submit" >Create Room</button>
       </form>
     );
