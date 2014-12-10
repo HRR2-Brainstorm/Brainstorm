a9ed8c9bfa757cc8cf808f41202a783560d448a6
Jason Deppen <jDeppen@gmail.com>
2014-12-10 13:47:26 -0500

    Finish socket.io for ideas, comments, and rooms.

--- server/server.js
+++ server/server.js
@@ -7,14 +7,21 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 io.on('connection', function(client) {
-  console.log('io says "Client connected..."');
-
-  client.on('messages', function(data) {
-    console.log(data);
-    client.broadcast.emit('messages', data);
+  client.on('comment-change', function(currentComments) {
+    client.broadcast.emit('comment-change', currentComments);
   });
 
-  client.emit('messages', 'hello world');
+  client.on('idea-change', function(currentIdeas) {
+    client.broadcast.emit('idea-change', currentIdeas);
+  });
+
+  client.on('interest-change', function(currentInterests) {
+    client.broadcast.emit('interest-change', currentInterests);
+  });
+
+  client.on('room-change', function(currentRooms) {
+    client.broadcast.emit('room-change', currentRooms);
+  });
 });
 
 require('./config/middleware')(app, express);
