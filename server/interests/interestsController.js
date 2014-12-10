a975f69fd5d21cee7f04c9e451dbe301fa78565d
brettdewoody <brett@digitalwaxworks.com>
2014-12-09 18:26:35 -0700

    Change interest.id to interest.idea

--- server/interests/interestsController.js
+++ server/interests/interestsController.js
@@ -4,7 +4,7 @@ var Q = require('q');
 module.exports = {
   add: function (req, res, next) {
     var interest = {};
-    interest.id = req.body.id;
+    interest.idea = req.body.id;
     interest.user = req.user._id;
 
     var addInterest = Q.nbind(Interests.create, Interests);
